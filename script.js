a77d4d2bdb93bcb59f7961ce10948100e6175bc8
Jessie Lyu <jiexinlyu@Jessies-MacBook-Pro-2.local>
2020-05-04 14:40:37 -0700

    website skeleton and d3 loading

---
+++ script.js
@@ -0,0 +1,16 @@
+
+/* Make the miles chart under statistics section */
+function milesChart() {
+    d3.select("#stats").style("color", "red")
+}
+
+/* Make the garbage dumped chart under statistics seciton */
+function garbageChart () {
+
+}
+
+$(document).ready(function () {
+    // D3 charts generation sequences go here
+    milesChart();
+    garbageChart();
+});
